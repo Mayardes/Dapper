889e0cfbf80813a2655f62f9763d64b653ef8d67
mayardes <mayardes.oliveira@globalsys.com.br>
2024-05-10 01:26:48 +0100

    feat: making a simple connection using ADO.net

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+const string connectionString = "Server=localhost,1433;Database=BaltaDapper;User Id=sa; Password=@Al126812";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+    Console.WriteLine("Connected!");
+
+    using (var command = new SqlCommand())
+    {
+        command.Connection = connection;
+        command.CommandType = CommandType.Text;
+        command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+        var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine($"Id: {reader.GetGuid(0)} Name: {reader.GetString(1)}");
+        }
+    }
+}
